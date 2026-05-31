098add76d6659e862effed78a2aafbb01753aa1c
Devanshu Yadav <devanshuyadav1611@gmail.com>
2026-05-31 21:37:16 +0530

    add render server details

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,22 +5,22 @@ const nextConfig = {
       // Route 1: PDF Uploads
       {
         source: '/api/upload/:path*',
-        destination: 'http://localhost:5000/api/upload/:path*',
+        destination: 'https://nickl-2-0.onrender.com/api/upload/:path*',
       },
       // Route 2: Dashboard & Database
       {
         source: '/api/portfolio/:path*',
-        destination: 'http://localhost:5000/api/portfolio/:path*',
+        destination: 'https://nickl-2-0.onrender.com/api/portfolio/:path*',
       },
       // Route 3: Live Market Data
       {
         source: '/api/market/:path*',
-        destination: 'http://localhost:5000/api/market/:path*',
+        destination: 'https://nickl-2-0.onrender.com/api/market/:path*',
       },
       // Route 4: Corporate Actions (Splits)
       {
         source: '/api/corporate/:path*',
-        destination: 'http://localhost:5000/api/corporate/:path*',
+        destination: 'https://nickl-2-0.onrender.com/api/corporate/:path*',
       }
       // we DO NOT include /api/auth here! Next.js will now handle authentication locally.
     ]
